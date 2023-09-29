13a08c98e1ff4d8ac4e4d0bf4f79f53768b4ae63
jpmakinen <pullasuti87@gmail.com>
2023-09-30 01:50:46 +0300

    color_palette_updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const updated = '20/09/2023';
+const updated = '30/09/2023';
 window.onload = function () {
+  // create and add stars
+  for (let i = 0; i <= 100; i++) {
+    const star = document.createElement('div');
+    star.className = 'stars';
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDelay = `${Math.random() * 10}s`; //  delay 10 seconds, 5 is too little
+    document.body.appendChild(star);
+  }
+
   const hiddenText = document.getElementById('textBox');
-//  hiddenText.style.display = 'none';
+  //  hiddenText.style.display = 'none';
 
   const updatedElements = document.getElementsByClassName('footer');
   for (const element of updatedElements) {
@@ -11,11 +21,11 @@ window.onload = function () {
   }
 
   document.getElementById('showButton').addEventListener('click', function () {
- hiddenText.classList.add("visible");
-//     if (hiddenText.style.display === 'none') {
-//       hiddenText.style.display = 'block';
-//     } else {
-//       hiddenText.style.display = 'none';
-//     }
+    hiddenText.classList.add('visible');
+    //     if (hiddenText.style.display === 'none') {
+    //       hiddenText.style.display = 'block';
+    //     } else {
+    //       hiddenText.style.display = 'none';
+    //     }
   });
 };
