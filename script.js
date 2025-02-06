cd4f1813c348ae59f78bc68dbc6575c4a793ccfe
pullasuti87 <pullasuti@gmail.com>
2025-02-06 22:46:40 +0000

    maybe_I_use_this

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const updated = "11/01/2024";
+const updated = '7/2/2025';
 const starsOn = false;
 
 window.onload = function () {
   if (starsOn) {
     // create and add stars
     for (let i = 0; i <= 100; i++) {
-      const star = document.createElement("div");
-      star.className = "stars";
+      const star = document.createElement('div');
+      star.className = 'stars';
       star.style.top = `${Math.random() * 100}%`;
       star.style.left = `${Math.random() * 100}%`;
       star.style.animationDelay = `${Math.random() * 10}s`; //  delay 10 seconds, 5 is too little
@@ -14,16 +14,22 @@ window.onload = function () {
     }
   }
 
-  const hiddenText = document.getElementById("textBox");
+  const hiddenBox = document.getElementById('text-box');
+  const hiddenAboutMe = document.getElementById('about-me');
+  const hiddenText = document.getElementById('about-me-text');
   //  hiddenText.style.display = 'none';
 
-  const updatedElements = document.getElementsByClassName("footer");
+  const updatedElements = document.getElementsByClassName('footer');
   for (const element of updatedElements) {
-    element.textContent = "updated: " + updated;
+    element.textContent = 'updated: ' + updated;
   }
 
-  document.getElementById("showButton").addEventListener("click", function () {
-    hiddenText.classList.add("visible");
+  document.getElementById('show-button').addEventListener('click', function () {
+    hiddenBox.style.display = 'block';
+    setTimeout(function () {
+      hiddenAboutMe.classList.add('visible');
+      hiddenText.classList.add('visible');
+    }, 250);
     //     if (hiddenText.style.display === 'none') {
     //       hiddenText.style.display = 'block';
     //     } else {
